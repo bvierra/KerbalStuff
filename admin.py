8630e1eab82f172d105845087d57e0b69940056d
Billy 'bvierra' Vierra <bvierra@cpan.org>
2015-04-09 16:49:02 -0700

    Added 'add_admin' function

--- admin.py
+++ admin.py
@@ -16,3 +16,14 @@ if sys.argv[1] == 'delete_user':
         db.commit()
         print("Success.")
         sys.exit()
+
+elif sys.argv[1] == 'add_admin':
+    user = User.query.filter(User.username == sys.argv[2]).first()
+    if not user:
+        sys.exit("User not found.")
+    else:
+        user.admin = True
+        user.confirmation = None
+        db.commit
+        print("Success: %s is now an admin" % user.username)
+        sys.exit()
